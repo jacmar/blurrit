ea46a44945d533b4fd5e2e871a5149d40006e5df
jacmar <jacopo.marello@gmail.com>
2025-04-11 12:47:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -570,7 +570,7 @@ def stack_images(images, blend_mode="average", randomness=0.5, seed=None):
         for i, img in enumerate(np_images):
             # Ridimensiona se necessario
             if img.shape[:2] != (height, width):
-    img = cv2.resize(img, (width, height))
+                img = cv2.resize(img, (width, height))
 # Ridimensiona se necessario
             if img.shape[:2] != (height, width):
                 img = cv2.resize(img, (width, height))
